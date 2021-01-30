4f13ab7fe3394ded45c8448f6579597afd20c885
Amirhossein <amir.h.nouri2000@gmail.com>
2021-01-30 20:03:08 +0330

    Router setup

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 // pages
-import Home from './pages/HomePage'
-import About from './pages/AboutPage'
-import Error from './pages/ErrorPage'
-import Products from './pages/ProductsPage'
-import SingleProduct from './pages/SingleProductPage'
-import Contact from './pages/ContactPage'
-import Cart from './pages/CartPage'
-
+import Home from "./pages/HomePage";
+import About from "./pages/AboutPage";
+import Error from "./pages/ErrorPage";
+import Products from "./pages/ProductsPage";
+import SingleProduct from "./pages/SingleProductPage";
+import Contact from "./pages/ContactPage";
+import Cart from "./pages/CartPage";
 
 function App() {
-  return <div>App</div>
+  return <Router>
+    <Switch>
+      <Route exact path="/" ><Home></Home></Route>
+      <Route exact path="/about" ><About></About></Route>
+      <Route exact path="/contact" ><Contact></Contact></Route>
+      <Route exact path="/products" ><Products></Products></Route>
+      <Route exact path="/product/:id" ><SingleProduct></SingleProduct></Route>
+      <Route exact path="/cart" ><Cart></Cart></Route>
+      <Route><Error></Error></Route>
+    </Switch>
+  </Router>;
 }
 
 export default App;
