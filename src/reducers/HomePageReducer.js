96612e3bb9841a89cfc6c41fa535103928d695fb
Amirhossein <amir.h.nouri2000@gmail.com>
2021-01-30 21:04:33 +0330

    CartItem added to HomePageReducer

--- src/reducers/HomePageReducer.js
+++ src/reducers/HomePageReducer.js
@@ -1,10 +1,15 @@
 const initialState = {
   sidebarOpen: false,
   cartOpen: false,
+  cartItems: 0,
 };
 
 const HomePageReducer = (state = initialState, action) => {
   switch (action.type) {
+    case "ADD_ITEM":
+      return { ...state, cartItems: state.cartItems + 1 };
+    case "REMOVE_ITEM":
+      return { ...state, cartItems: state.cartItems - 1 };
     case "TOGGLE_CART":
       return { ...state, cartOpen: !state.cartOpen };
     case "TOGGLE_SIDEBAR":
