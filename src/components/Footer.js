023d68e229339207a93be56497ec5d84856f2318
Amirhossein <amir.h.nouri2000@gmail.com>
2021-01-31 20:17:33 +0330

    Footer structured

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import styled from 'styled-components'
+import {useSelector} from 'react-redux'
 
 export default function Footer() {
+    const {socialData} = useSelector(state => state.homePage)
     return (
-        <div>
-            Footer
-        </div>
+        <Wrapper>
+            <div className="footer__title">
+                <p className="text-capitalize">
+                    copyright &copy; tech store {new Date().getFullYear()}. all rights reserved
+                </p>
+            </div>
+            <div className="footer__icons">
+                {socialData.map((item) =>{
+                    return <a key={item.id} href={item.url}>{item.icon}</a>
+                })}
+            </div>
+        </Wrapper>
     )
 }
+const Wrapper = styled.footer`
+    
+`
