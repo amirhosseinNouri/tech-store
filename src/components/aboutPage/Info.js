dbd2245462d2bedd05a2ecdeef1ed9da4343989c
Amirhossein <amir.h.nouri2000@gmail.com>
2021-01-31 14:48:16 +0330

    AboutPage styled

--- src/components/aboutPage/Info.js
+++ src/components/aboutPage/Info.js
@@ -1,35 +1,60 @@
 import React from "react";
 import Title from "../Title";
 import aboutBcg from "../../images/aboutBcg.jpeg";
+import styled from "styled-components";
 
 export default function Info() {
   return (
-    <section className="py-5">
-      <div className="container">
-        <div className="row">
-          <div className="col-10 mx-auto col-md-6 my-3">
-            <img
-              src={aboutBcg}
-              className="img-fluid img-thumbnail"
-              alt="about company"
-            />
-          </div>
-          <div className="col-10 mx-auto col-md-6 my-3">
-            <Title title="about us"></Title>
-            <p className="text-lead text-muted my-3">
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id sint
-              inventore doloremque dicta facere aliquid corrupti maiores
-              commodi, minima nobis.
-            </p>
-            <p className="text-lead text-muted my-3">
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id sint
-              inventore doloremque dicta facere aliquid corrupti maiores
-              commodi, minima nobis.
-            </p>
-            <button className="btn" type="button">More info</button>
-          </div>
-        </div>
+    <Wrapper>
+      <div>
+        <img
+          src={aboutBcg}
+          className="img-fluid img-thumbnail"
+          alt="about company"
+        />
       </div>
-    </section>
+      <div className="title-container">
+        <Title title="about us"></Title>
+        <p className="text-lead text-muted my-3">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id sint
+          inventore doloremque dicta facere aliquid corrupti maiores commodi,
+          minima nobis.
+        </p>
+        <p className="text-lead text-muted my-3">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id sint
+          inventore doloremque dicta facere aliquid corrupti maiores commodi,
+          minima nobis.
+        </p>
+        <button className="btn info-btn" type="button">
+          More info
+        </button>
+      </div>
+    </Wrapper>
   );
 }
+
+const Wrapper = styled.section`
+  padding : 5rem 6rem; 
+  display : grid ;
+  grid-template-columns : 1fr;
+  gap : 0.8rem ;
+
+  img{
+    width : 90%;
+    display : block;
+    border : 4px solid var(--mainBlack);
+  }
+
+  .title-container p {
+    margin-top : 1rem;
+  }
+
+  .info-btn {
+    margin : 2rem 0 ;
+  }
+
+  @media (min-width : 768px){
+    grid-template-columns : 1fr 1fr;
+  }
+  
+`;
