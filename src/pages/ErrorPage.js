f6c27d5b9f755ac6f4bb7a0479ccbb446f33cc82
Amirhossein <amir.h.nouri2000@gmail.com>
2021-01-31 09:15:06 +0330

    Error page completed

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+import Hero from "../components/Hero";
+import defaultBcg from "../images/defaultBcg.jpeg";
+import {Link} from 'react-router-dom'
 
 export default function Error() {
-    return (
-        <div>
-            Error Page
-        </div>
-    )
+  return <Hero img={defaultBcg} title="404" max="true">
+      <h2 className="text-uppercase">Page not found</h2>
+      <Link to="/" className="btn">Return home</Link>
+  </Hero>
 }
+
