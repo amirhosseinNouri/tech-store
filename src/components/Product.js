2738bee4ec0e18dd6bc7ee6fc6bd6899def670ad
Amirhossein <amir.h.nouri2000@gmail.com>
2021-02-01 12:45:20 +0330

    Product component styled

--- src/components/Product.js
+++ src/components/Product.js
@@ -14,19 +14,23 @@ export default function Product({ product }) {
       </div>
       <div className="product__info">
         <span>{product.title}</span>
-        <span>{product.price}</span>
+        <span className="product__price">{product.price}</span>
       </div>
     </Wrapper>
   );
 }
 
 const Wrapper = styled.article`
-  padding: 2rem 2rem 1rem 2rem;
+  padding: 2.5rem 2rem 1.5rem 2rem;
   background-color: var(--mainWhite);
   border: 1px solid #ccc;
   width: 250px;
   border-radius: 5px;
-  margin-bottom : 2rem;
+  margin-bottom: 2rem;
+  box-shadow: 0 8px 24px 0 rgba(0, 0, 0, 0.15);
+
+  transition: var(--mainTransition);
+  color: var(--mainBlack);
 
   .product__img {
     margin-bottom: 2.5rem;
@@ -38,6 +42,17 @@ const Wrapper = styled.article`
   .product__info {
     display: flex;
     justify-content: space-between;
-    align-items : center;
+    align-items: center;
+
+    span {
+      font-size: 0.9rem;
+    }
+    .product__price {
+      color: var(--primaryColor-2);
+    }
+  }
+
+  &:hover {
+    box-shadow: 2px 2px 10px 0 rgba(0, 0, 0, 0.2);
   }
 `;
