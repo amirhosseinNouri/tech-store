b9e847afaf3de373496687e34b04790bd7746f78
Amirhossein <amir.h.nouri2000@gmail.com>
2021-02-03 05:47:22 +0330

    No item match filtering parameters handled

--- src/components/productsPage/Products.js
+++ src/components/productsPage/Products.js
@@ -3,17 +3,29 @@ import styled from "styled-components";
 import { useSelector } from "react-redux";
 import Product from "../Product";
 import Title from "../Title";
+import Filter from "../Filter";
 
 export default function Products() {
-  const { storeProducts } = useSelector((state) => state.product);
+  const { storeProducts, filteredProducts } = useSelector(
+    (state) => state.product
+  );
   return (
     <Wrapper>
       <Title title="our products" center="true"></Title>
-      <div className="products">
-        {storeProducts.map((item) => {
-          return <Product product={item} key={item.id}></Product>;
-        })}
-      </div>
+      <Filter></Filter>
+      <h6 className="filter-number">
+        Total products : {filteredProducts.length}
+      </h6>
+
+      {filteredProducts.length === 0 ? (
+        <h4 className="no-items">Sorry, no items matched your search.</h4>
+      ) : (
+        <div className="products">
+          {storeProducts.map((item) => {
+            return <Product product={item} key={item.id}></Product>;
+          })}
+        </div>
+      )}
     </Wrapper>
   );
 }
@@ -27,7 +39,18 @@ const Wrapper = styled.section`
     display: grid;
     grid-template-columns: repeat(auto-fit, minmax(250px, auto));
     row-gap: 2rem;
-    column-gap : 3rem;
+    column-gap: 3rem;
     max-width: 920px;
+  }
+
+  .filter-number {
+    font-size: 1rem;
+    padding: 2rem 0;
+    border-bottom: 1px solid #ccc;
+  }
+
+  .no-items {
+    margin-top: 4rem;
+    font-size: 1.2rem;
   }
 `;
